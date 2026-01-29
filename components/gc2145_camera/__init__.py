8072c82dc8f55f578fd40e2732b2b11e96349d49
yanyuq <yanyq@leadingtek.com>
2026-01-29 21:50:31 +0800

    GC2145_camera

--- components/gc2145_camera/__init__.py
+++ components/gc2145_camera/__init__.py
@@ -7,7 +7,13 @@ DEPENDENCIES = ['esp32']
 
 # 定义命名空间
 gc2145_ns = cg.esphome_ns.namespace('gc2145_camera')
-GC2145Camera = gc2145_ns.class_('GC2145Camera', camera.Camera, cg.Component)
+
+# 【修复关键点】手动获取 Camera 基类的 C++ 引用
+camera_ns = cg.esphome_ns.namespace('camera')
+Camera = camera_ns.class_('Camera', cg.Entity)
+
+# 定义我们的类，继承自 Camera 和 Component
+GC2145Camera = gc2145_ns.class_('GC2145Camera', Camera, cg.Component)
 
 # 配置模式
 CONFIG_SCHEMA = camera.CAMERA_SCHEMA.extend({
@@ -19,6 +25,7 @@ CONFIG_SCHEMA = camera.CAMERA_SCHEMA.extend({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    # 注册摄像头
     await camera.register_camera(var, config)
     
     # 将翻转参数传递给 C++
