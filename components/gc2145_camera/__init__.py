45fb2ce4b392c79bb6b703612292b4355b3b5a74
yanyuq <yanyq@leadingtek.com>
2026-01-30 12:15:58 +0800

    custom component

--- components/gc2145_camera/__init__.py
+++ components/gc2145_camera/__init__.py
@@ -3,21 +3,16 @@ import esphome.config_validation as cv
 from esphome.components import camera
 from esphome.const import CONF_ID, CONF_NAME
 
-DEPENDENCIES = ['esp32']
+# 【关键】必须依赖 camera，否则 C++ 找不到头文件
+DEPENDENCIES = ['esp32', 'camera']
 
 gc2145_ns = cg.esphome_ns.namespace('gc2145_camera')
-camera_ns = cg.esphome_ns.namespace('camera')
 
-# 1. 安全获取 Entity 基类
-try:
-    Entity = cg.Entity
-except AttributeError:
-    Entity = cg.esphome_ns.class_('Entity')
-
-Camera = camera_ns.class_('Camera', Entity)
+# 只要缓存清理干净，官方的 Camera 类是可以直接获取的
+# 如果这里还报错，说明缓存没清干净
+Camera = camera.Camera
 GC2145Camera = gc2145_ns.class_('GC2145Camera', Camera, cg.Component)
 
-# 2. 手动定义配置 Schema
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(GC2145Camera),
     cv.Optional(CONF_NAME): cv.string,
@@ -27,17 +22,12 @@ CONFIG_SCHEMA = cv.Schema({
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    
-    # 注册组件 (Component)
     await cg.register_component(var, config)
     
-    # 【修复重点】
-    # 不再调用 await camera.register_camera(var, config)
-    # 而是手动设置 Name，这样即使 camera 库有问题也能绕过
-    if CONF_NAME in config:
-        cg.add(var.set_name(config[CONF_NAME]))
+    # 【关键】缓存清理干净后，官方组件肯定有 register_camera 方法
+    # 这会配置好所有的 camera 基础参数
+    await camera.register_camera(var, config)
 
-    # 应用翻转设置
     if "vertical_flip" in config:
         cg.add(var.set_vflip(config["vertical_flip"]))
     if "horizontal_mirror" in config:
